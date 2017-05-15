56232a23a27db992fd21d9a0383bcfd06919cd9b
Schneider456 <efrain_acdc@hotmail.com>
2017-05-14 22:12:49 -0500

    Lecciones y Ejercicios

---
+++ CodeLine_v/src/CodeLine/Ejercicio.java
@@ -0,0 +1,15 @@
+package CodeLine;
+
+public class Ejercicio {
+    
+    private String ejercicio;
+    private String retroalimentacion;
+    
+    public void añadeEjercicio (String ejercicio) {
+        this.ejercicio = ejercicio;
+    }
+    
+    public String getretroalimentacion () {
+        return retroalimentacion;
+    }
+}
